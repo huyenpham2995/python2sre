b226716e50218e857a98bfb7834d4c7e7d8c9fd5
Huyen Pham <thanhhuyen2995@gmail.com>
2021-07-18 22:21:57 +0000

    Using pytest for unit test

--- week1/plus_test.py
+++ week1/plus_test.py
@@ -1,24 +1,12 @@
-from absl.testing import absltest
-from absl.testing import parameterized
+import pytest
 from plusxy import findSum
 
-class UnitTests(parameterized.TestCase):
-	@parameterized.named_parameters(
-		{
-			'testcase_name': 'plus1',
-          		'input1': 3,
-          		'input2': 5,
-          		'want': 8,
-      		},
-      		{
-          		'testcase_name': 'plus2',
-          		'input1': 6,
-          		'input2': -2,
-          		'want': 4,
-      		})
+def test_positive():
+	assert findSum(3, 5) == 8
 
-	def testSum(self, input1, input2, want):
-    		self.assertEqual(findSum(input1, input2), want)	
+def test_negative():
+	assert findSum(6, -2) == 4
 
-if __name__ == '__main__':
-    absltest.main()
+def test_decimal():
+	assert findSum(1.4, 2.2) == 3.6
+
