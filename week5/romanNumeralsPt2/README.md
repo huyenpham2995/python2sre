6109a8f95eba4ce193f4f61a09cc9eb53429b71a
Huyen Pham <thanhhuyen2995@gmail.com>
2021-08-30 00:00:14 +0000

    done with roman numerals pt2

--- week5/romanNumeralsPt2/README.md
+++ week5/romanNumeralsPt2/README.md
@@ -11,14 +11,14 @@
     - We see I next. I < X, I < sum => add I to the sum (sum = 15+1 = 16).
     - Return 16.
 - There are 2 cases where we need to deduct:
-    - If the current char value > the previous char value (XIV) => next char - current char. Eg: XIV.
+    - If the current char value > the previous char value (XIV) => cur char - previous char. Eg: XIV.
         - We see X, add it to sum (sum = 10).
         - We see I, I < X => add I to sum (sum = 11).
         - We see V, V > I:
             - V - I = 4. Add this value to sum (sum = 11+4 = 15)
             - Deduct I from the sum so we don't count for it twice (sum = 15-1 = 14).
         - Return 14
-    - If the current sum is less than the next char value => next char - current sum. (IVX)
+    - If the current sum is less than the next char value => cur char - current sum. (IVX)
         - We see I, add it to sum (sum = 1).
         - We see V, V > sum:
             - V - sum = 5-1 = 4. Add this value to sum (sum = 4+1 = 5).
--- week5/romanNumeralsPt2/romanNumeralsPt2.py
+++ week5/romanNumeralsPt2/romanNumeralsPt2.py
@@ -1,33 +1,36 @@
 romanNumeralsDict= {"I":1, "V":5, "X":10, "L":50, "C": 100, "D":500, "M":1000}
 
-def romanNumeralsPt2(string):
+def romanNumeralsPt2(s: str) -> int:
     # Empty string is invalid
-    if len(string) == 0:
+    if len(s) == 0:
         return None
     
     # If there's only 1 letter, see if the letter is valid. 
     # If it is return the value
-    string = string.upper()
+    s = s.upper()
     result = 0
     index = 0
 
-    while index < len(string):
+    while index < len(s):
         # invalid if character does not exist in the dictionary
-        if string[index] not in romanNumeralsDict.keys():
+        if s[index] not in romanNumeralsDict.keys():
             return None
 
-        # first character will be added to the result sum automatically
+        # first one is auto added to the result
         if index == 0:
-            result += romanNumeralsDict[string[index]]
+            result += romanNumeralsDict[s[index]]
         else:
-            prevNum = romanNumeralsDict[string[index-1]]
-            curNum = romanNumeralsDict[string[index]]
-            if prevNum < curNum:
-                result -= prevNum
-                result += curNum - prevNum
+            prevNum = romanNumeralsDict[s[index-1]]
+            curNum = romanNumeralsDict[s[index]]
+            if curNum < prevNum:
+                result += curNum
             else:
-                result += curNum 
-
-        index += 1    
+                if curNum > result:
+                    result = curNum - result
+                else:
+                    result -= prevNum
+                    result += curNum - prevNum
+        
+        index += 1
 
     return result
