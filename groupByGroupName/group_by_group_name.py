f9ec35d4071949dae7f9645c8c0185f5219ef2b5
Huyen Pham <thanhhuyen2995@gmail.com>
2025-01-08 22:23:32 -0800

    Finished group by group name

--- groupByGroupName/group_by_group_name.py
+++ groupByGroupName/group_by_group_name.py
@@ -1,2 +1,24 @@
+from collections import defaultdict
+
 def groupByGroupName(inputFileName: str):
-    return
+    lines = defaultdict(list)
+    
+    try:
+        with open(inputFileName,'r') as inputf:
+            for line in inputf:
+                line = line.strip()
+                words = line.split()
+                if len(words) < 4:
+                    raise ValueError("Input is not in correct format")
+                lines[words[1]].append(words)
+        
+        with open('group_by_name_output.txt', 'w') as outputf:
+            for key in sorted(lines.keys()):
+                for words in sorted(lines[key], key=lambda x:x[2]):
+                    outputf.write(" ".join(words))
+                    outputf.write("\n")
+                    
+    except IOError as e:
+        print(e)
+
+groupByGroupName("group_by_group_name_test_input.txt")
